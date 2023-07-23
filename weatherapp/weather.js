c989388c52ed7f1356485a3ec31ef531a0e3790e
Durgeshpratapsingh123 <113275255+Durgeshpratapsingh123@users.noreply.github.com>
2023-07-23 21:35:10 +0530

    first commit

---
+++ weatherapp/weather.js
@@ -0,0 +1,66 @@
+const apiKey = "70f005e3ee056d49c041342f6c041cea";
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+async function getWeatherByLocation(city){
+
+    const resp = await fetch(url(city) ,{
+        origin: "cros"
+    });
+
+    const respData = await resp.json();
+
+    addWeatherToPage(respData);
+}
+
+function addWeatherToPage(data){
+    const temp = Ktoc(data.main.temp);
+
+    const weather = document.createElement('div')
+    weather.classList.add('weather');
+
+    weather.innerHTML=`
+    <h1 ><img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" style={color: red} /> ${temp}°C <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" /></h1>
+    <small>${data.weather[0].main}</small>`;
+
+    main.innerHTML = "";
+    main.appendChild(weather);
+};
+
+function Ktoc(K){
+    return Math.floor(K - 273.15);
+}
+
+form.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    const city = search.value;
+
+    if(city){
+        getWeatherByLocation(city)
+    }
+})
+
+
+
+
+function getCurrentDateTime() {
+    const currentDate = new Date();
+    const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const timeOptions = { hour: 'numeric', minute: 'numeric', hour12: true };
+    const formattedDate = currentDate.toLocaleDateString('en-US', dateOptions);
+    const formattedTime = currentDate.toLocaleTimeString('en-US', timeOptions);
+    return `${formattedDate} - ${formattedTime}`;
+}
+
+function updateDateTime() {
+    const dateTimeElement = document.getElementById('date-time');
+    dateTimeElement.textContent = getCurrentDateTime();
+}
+
+updateDateTime();
+
+setInterval(updateDateTime, 60000); 
